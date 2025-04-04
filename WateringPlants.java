2b10509e572cfce7727e2b4aa5c7b526de3da8cb
gargi <gargi.p.sukhatankar@gmail.com>
2025-04-04 23:52:28 +0530

    added code

---
+++ WateringPlants.java
@@ -0,0 +1,25 @@
+public class WateringPlants {
+    public static int stepCalculator(int[] arr, int capacity){
+        int i=-1;
+        int length = arr.length;
+        int tempCap = capacity;
+        int stepCount = 0;
+        while(i<length-1){
+            if(arr[i+1]>tempCap){
+                stepCount = stepCount + 2*(i-(-1));
+                tempCap = capacity;
+            }
+            i++;
+            stepCount++;
+            tempCap-=arr[i];
+        }
+        return stepCount;
+    }
+
+    public static void main(String[] args) {
+        int[] plants = {1,1,1,4,2,3};
+        int capacity = 4;
+        int steps = stepCalculator(plants, capacity);
+        System.out.println(steps);
+    }
+}
