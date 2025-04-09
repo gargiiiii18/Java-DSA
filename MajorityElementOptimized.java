daae9fa40323c6682fe68f117dda5a117e482da3
gargi <gargi.p.sukhatankar@gmail.com>
2025-04-09 23:03:18 +0530

    added majority element code

---
+++ MajorityElementOptimized.java
@@ -0,0 +1,26 @@
+public class MajorityElementOptimized {
+
+    public static int calculateMajorityElement(int[] arr){
+        int length = arr.length;
+        int threshold = length/2;
+        int candidate=0;
+        int count=0;
+        for(int i=0; i<length; i++){
+            if(arr[i]==arr[candidate]){
+                count++;
+            } else{
+                count--;
+                if(count<0){
+                    candidate=i;
+                }
+            }
+        }
+        return arr[candidate];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        int majorityElement = calculateMajorityElement(nums);
+        System.out.println(majorityElement);
+    }
+}
