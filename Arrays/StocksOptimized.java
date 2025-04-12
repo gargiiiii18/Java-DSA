8c4c154d9cf6f2fee7764b981ada65683554748a
gargi <gargi.p.sukhatankar@gmail.com>
2025-04-13 01:47:38 +0530

    buying and selling stocks most optimized

---
+++ Arrays/StocksOptimized.java
@@ -0,0 +1,28 @@
+package Arrays;
+import java.util.Arrays;
+public class StocksOptimized{
+
+    public static int calculateProfit(int[] arr){
+        int length = arr.length;
+        int min = 0;
+        int profit = 0;
+        int maxProfit = 0;
+        for(int i=0; i<length; i++){
+            if(arr[i]<arr[min]){
+                min = i;
+            }
+          profit = arr[i] - arr[min];
+            if(profit>maxProfit){
+                maxProfit = profit;
+            }
+
+        }
+    return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,6,4,3,1};
+        int profit = calculateProfit(prices);
+        System.out.println(profit);
+    }
+}
