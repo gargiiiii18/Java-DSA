e4c545abff1f2ef455b7666f55144ed3882bc830
gargi <gargi.p.sukhatankar@gmail.com>
2025-04-13 17:05:05 +0530

    GCD of strings proper logic

--- Strings/GCDStringsOptimized.java
+++ Strings/GCDStringsOptimized.java
@@ -1,10 +1,26 @@
 package Strings;
 public class GCDStringsOptimized {
-    public static String findGCDSubstring(String s, String t){
-
+    public static boolean checkGCDSubstringPossible(String s, String t){
+        String concat1 = s + t;
+        String concat2 = t + s;
+        if(!concat1.equals(concat2)){
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-
+        String str1 = "ababab";
+        String str2 = "abab";
+        boolean GCDPossible = checkGCDSubstringPossible(str1, str2);
+        if(GCDPossible){
+            if(str1.length()<str2.length()){
+                System.out.println(str1);
+            } else{
+                System.out.println(str2);
+            }
+        } else{
+            System.out.println("No GCD is possible");
+        }
     }
 }
