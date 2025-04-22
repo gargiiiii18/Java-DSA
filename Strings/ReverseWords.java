126cc46ce1455a24271f857d6cd9f1351fe58948
gargi <gargi.p.sukhatankar@gmail.com>
2025-04-22 18:19:30 +0530

    Updated code for reversing words

--- Strings/ReverseWords.java
+++ Strings/ReverseWords.java
@@ -1,10 +1,50 @@
-public class ReverseWords {
+public class ReverseWords{
 
-    public static String reverse(String word){
+    public static StringBuilder createStringBuilder(String s){
+//        System.out.println("creating stringbuilder");
+        s = s.trim();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            sb.append(s.charAt(i));
+        }
+        return sb;
+    }
+
+    public static void reverseEntireSequence(StringBuilder sb, int left, int right){
+//        System.out.println("im called");
+        while(left<right){
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left++;
+            right--;
+        }
 
     }
 
-    public static void main(String[] args) {
+    public static String reverseWords(String s){
+        int start = 0;
+        int end = 0;
 
+        StringBuilder sb = createStringBuilder(s);
+        int length = sb.length();
+        reverseEntireSequence(sb, 0, length-1);
+//        System.out.println(sb.toString());
+
+        while(start<length){
+            while(end<length && sb.charAt(end) != ' '){
+               end++;
+            }
+            reverseEntireSequence(sb, start, end-1);
+            start = end+1;
+            end = start;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        String reversed = reverseWords(s);
+        System.out.println(reversed);
     }
 }
