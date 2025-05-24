0d41dfa3d89287ccd34921bdd61441995c82d1a0
gargi <gargi.p.sukhatankar@gmail.com>
2025-05-25 00:30:13 +0530

    Deleted file

---
+++ Sorting/Matches.java
@@ -0,0 +1,13 @@
+package Sorting;
+import java.util.ArrayList;
+import java.util.List;
+public class Matches{
+
+    public static List<Integer> calculateWinsLosses(List<Integer> matches){
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> matches = 
+    }
+}
