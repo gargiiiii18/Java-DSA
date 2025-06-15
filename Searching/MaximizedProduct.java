2eb4c5d52de3c9553460efeffacba70e5c86f629
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-15 17:25:52 +0530

    Added Maximize Product code

---
+++ Searching/MaximizedProduct.java
@@ -0,0 +1,42 @@
+package Searching;
+
+public class MaximizedProduct {
+
+    public static int minimizeMaxProduct(int n, int[] qtys){
+        int low = 0;
+        int high = Integer.MIN_VALUE;
+        int result = 0;
+
+        for(int qty : qtys){
+            if(qty>high){
+                high=qty;
+            }
+        }
+
+        while(low<=high){
+            int stores = 0;
+            int mid = (low+high)/2;
+
+            for(int qty : qtys){
+                stores += qty/mid;
+                if(qty%mid>0){
+                    stores++;
+                }
+            }
+            if(stores<=n){
+                result = mid;
+                high = mid-1;
+            } else{
+                low=mid+1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] quantities = {11, 6};
+        int n = 6;
+        int result = minimizeMaxProduct(n, quantities);
+        System.out.println(result);
+    }
+}
