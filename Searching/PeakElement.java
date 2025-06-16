80922569fce2a787b5c291683e6cd7c9e8f7953e
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-16 20:26:34 +0530

    Added finding peak element code

---
+++ Searching/PeakElement.java
@@ -0,0 +1,29 @@
+package Trials.Searching;
+
+public class PeakElement {
+
+    public static int findPeak(int[] nums){
+      int left = 0;
+      int right = nums.length-1;
+      while(left<right){
+           int mid = left + (right-left)/2;
+     
+           if(nums[mid]>nums[mid+1]){
+                right=mid;
+           } else {
+                left=mid+1;
+           }
+          
+      }
+      return left;
+    }
+    
+
+    public static void main(String[] args) {
+     //    int[] nums = {1, 4, 1, 3};
+        int[] nums = {2, 1, 1, 1};
+        int peak = findPeak(nums);
+
+        System.out.println(peak);
+    }
+}
