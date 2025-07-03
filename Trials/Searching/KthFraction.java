635b1d3ee39a48a38436a7d300c2e96f95711f74
gargi <gargi.p.sukhatankar@gmail.com>
2025-07-04 02:01:29 +0530

    added kth fraction code

---
+++ Trials/Searching/KthFraction.java
@@ -0,0 +1,83 @@
+// You are given a sorted integer array arr containing 1 and prime numbers, where all the integers of arr are unique. You are also given an integer k.
+
+// For every i and j where 0 <= i < j < arr.length, we consider the fraction arr[i] / arr[j].
+
+// Return the kth smallest fraction considered. Return your answer as an array of integers of size 2, where answer[0] == arr[i] and answer[1] == arr[j].
+
+// Constraints:
+
+// 2 <= arr.length <= 1000
+// 1 <= arr[i] <= 3 * 10^4
+// arr[0] == 1
+// arr[i] is a prime number for i > 0.
+// All the numbers of arr are unique and sorted in strictly increasing order.
+// 1 <= k <= arr.length * (arr.length - 1) / 2
+
+// Example 1:
+
+// Input: arr = [1,2,3,5], k = 3
+// Output: [2,5]
+// Explanation: The fractions to be considered in sorted order are:
+// 1/5, 1/3, 2/5, 1/2, 3/5, and 2/3.
+// The third fraction is 2/5.
+
+// Example 2:
+
+// Input: arr = [1,7], k = 1
+// Output: [1,7]
+
+package Trials.Searching;
+
+import java.util.Arrays;
+
+public class KthFraction {
+
+    public static int[] findKthFrac(int[] input, int k){
+        double left = 0.0;
+        double right = 1.0;
+        double maxFrac = 0.0;
+        int[] curr = new int[2];
+
+        while(left<right){
+            double mid = (left+right)/2;
+            int count = 0;
+             int i = 0;
+             int j = i + 1;
+        while(i < input.length && j < input.length){
+            double frac = (double) input[i]/input[j];
+                if(frac < mid){
+                    count++;
+                    if(frac > maxFrac){
+                        maxFrac = frac;
+                        curr[0] = input[i];
+                        curr[1] = input[j];
+                    }
+                    if(j+1<input.length){
+                        j++;
+                    } else{
+                        i++;
+                        j=i+1;
+                    }
+                } else {
+                    j++;
+                }
+            }
+            if(count == k){
+                    return curr;
+                } else if(count<k){
+                    left = mid;
+                } else{
+                    right = mid;
+                }
+        }
+            return new int[] {};
+        }
+    
+
+    public static void main(String[] args) {
+        int[] input = {1, 2, 3, 5, 7, 11, 13, 17};
+        int k = 10;
+        int[] result = findKthFrac(input, k);
+        System.out.println(Arrays.toString(result));
+    }
+}
