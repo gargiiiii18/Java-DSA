24b3f0cc97e57027a4b45c4aca5ea92b6862ed1a
gargi <gargi.p.sukhatankar@gmail.com>
2025-07-28 02:23:08 +0530

    added sorting techniques

---
+++ Sorting/SortingTechniques/BubbleSort.java
@@ -0,0 +1,34 @@
+package Sorting.SortingTechniques;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr){
+        for(int i=0; i<arr.length; i++) {
+            boolean sorted = false;
+            for(int j=0; j<arr.length-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                sorted = true;
+            }
+        }
+        if(!sorted){
+            break;
+        }
+    }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 3, 8, 4, 2};
+
+        System.out.println(Arrays.toString(arr));
+
+        bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
