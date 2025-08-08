337cecd56f6d8c43347a8afc570949c18d34c46a
gargi <gargi.p.sukhatankar@gmail.com>
2025-08-08 21:41:00 +0530

    added digital root code using recursion

--- Recursion/DigitalRoot.java
+++ Recursion/DigitalRoot.java
@@ -1,3 +1,22 @@
+// You are given a number n. You need to find the digital root of n.
+// DigitalRoot of a number is the recursive sum of its digits until we get a single digit number.
+
+// Constraints:
+// 1 <= n <= 10^7
+
+// Example 1:
+// Input:
+// n = 1
+// Output: 1
+// Explanation: Digital root of 1 is 1
+
+// Example 2:
+// Input:
+// n = 99999
+// Output: 9
+// Explanation: Sum of digits of 99999 is 45, which is not a single digit number, hence sum of digit of 45 is 9 which is a single digit number.
+
+
 package Recursion;
 
 public class DigitalRoot {
