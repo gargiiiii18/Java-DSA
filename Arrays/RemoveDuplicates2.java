422051ff82ae4c4edba8b810c03ed943c8f43de2
gargi <gargi.p.sukhatankar@gmail.com>
2025-09-23 23:52:26 +0530

    added remove duplicates II code leet 80

---
+++ Arrays/RemoveDuplicates2.java
@@ -0,0 +1,35 @@
+package Arrays;
+import java.util.Arrays;
+
+public class RemoveDuplicates2 {
+
+    public int removeDuplicates(int[] nums){
+
+        int duplicate = 0;
+        int writeIdx = 1;
+ 
+        for(int j=1; j<nums.length; j++){
+            if(nums[j] == nums[j-1]){
+                duplicate++;
+            }
+            if(nums[j]!=nums[j-1]){
+                duplicate = 0;
+            }             
+            if(duplicate<1){
+                nums[writeIdx++] = nums[j];                
+            }            
+
+        }
+        System.out.println(Arrays.toString(nums));
+        return writeIdx;
+    }
+
+    public static void main(String[] args) {
+        // int[] nums = {1,1,1,2,2,3};
+        // int[] nums = {0,0,1,1,1,1,2,3,3};
+        int[] nums = {1,1,1,1};
+        RemoveDuplicates2 rm = new RemoveDuplicates2();
+        int res = rm.removeDuplicates(nums);
+        System.out.println(res);
+    }
+}
